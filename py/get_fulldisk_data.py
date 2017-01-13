8242ef68f5197f7b9fadf1fce87783654896d118
Jack Ireland <jackireland@gmail.com>
2017-01-13 11:13:00 -0500

    converting the jet information from lists into an object

--- py/get_fulldisk_data.py
+++ py/get_fulldisk_data.py
@@ -8,18 +8,11 @@ from sunpy.time import parse_time
 # Have the necessary data already been downloaded?
 data_already_downloaded = False
 
-# Better if the number of pixels in each direction is a power of two
-dyadic = True
-if dyadic:
-    dyadic = 8
-side_length_in_pixels = 300.0 * u.pix
-
 # Where to save the FITS data
 save_location = os.path.expanduser('~/Data/jets/2012-11-20/')
 
 # Where to save the images
 img_location = os.path.expanduser('~/jets/img/2012-11-20/')
-
 
 # Which data to download
 observables = [[vso.attrs.Instrument('hmi'), vso.attrs.Physobs('LOS_magnetic_field')],
@@ -30,37 +23,32 @@ observables = [[vso.attrs.Instrument('hmi'), vso.attrs.Physobs('LOS_magnetic_fie
                [vso.attrs.Instrument('aia'), vso.attrs.Wave(211*u.AA, 211*u.AA)], 
                [vso.attrs.Instrument('aia'), vso.attrs.Wave(335*u.AA, 335*u.AA)], ]
 
-# Jet times
-jet_observation_times = [('2012-11-20 01:28', '2012-11-20 01:30'),
-                         ('2012-11-20 02:31', '2012-11-20 02:33'),
-                         ('2012-11-20 04:34', '2012-11-20 04:36'),
-                         ('2012-11-20 06:08', '2012-11-20 06:10')]
 
-jet_observation_times = [('2012-11-20 01:28', '2012-11-20 01:30')]
+class Jet:
+    def __init__(self, start_time, end_time, position, name=None,
+                 time_window_half_length=20*u.minute,
+                 side_length_in_pixels=256*u.pix):
+        self.start_time = start_time
+        self.end_time = end_time
+        self.position = position
+        self.name = name
+        self.time_window_half_length = time_window_half_length
+        self.side_length_in_pixels = side_length_in_pixels
 
-# Half length of the window around the main observation times
-jet_times_window_half_length = 20*u.minute
+jet0 = Jet('2012-11-20 01:28', '2012-11-20 01:30', [790, -313] * u.arcsec,
+           name='jet_0')
 
-# Create the jet observation times
-jet_times = []
-for jot in jet_observation_times:
-    jts = parse_time(jot[0]) - datetime.timedelta(seconds=jet_times_window_half_length.to(u.s).value)
-    jte = parse_time(jot[1]) + datetime.timedelta(seconds=jet_times_window_half_length.to(u.s).value)
-    jet_times.append(vso.attrs.Time(jts, jte))
+jet1 = Jet('2012-11-20 02:31', '2012-11-20 02:33', [3, 4] * u.arcsec,
+           name='jet_1')
 
+jet2 = Jet('2012-11-20 04:34', '2012-11-20 04:36', [5, 6] * u.arcsec,
+           name='jet_2')
 
-# Jet positions
-jet_positions = [[790, -313] * u.arcsec,
-                 [3, 4] * u.arcsec,
-                 [5, 6] * u.arcsec,
-                 [7, 8] * u.arcsec]
+jet3 = Jet('2012-11-20 06:08', '2012-11-20 06:10', [7, 8] * u.arcsec,
+           name='jet_3')
 
-jet_positions = [[790, -313] * u.arcsec]
-
-if dyadic:
-    side_length_in_pixels = u.pix * 2 ** dyadic
-print('Side length = ', side_length_in_pixels)
-
+# List of jets
+jets = [jet0, jet1, jet2, jet3]
 
 # Data acquisition
 if not data_already_downloaded:
@@ -71,15 +59,19 @@ if not data_already_downloaded:
     qr = []
 
     # Go through all the jet times
-    for j, times in enumerate(jet_times):
-        print('Time number {:n} out of {:n}'.format(j, len(jet_times)))
+    for j, jet in enumerate(jets):
+        print('Time number {:n} out of {:n}'.format(j, len(jets)))
+        seconds = jet.time_window_half_length.to(u.s).value
+        jts = parse_time(jet.start_time) - datetime.timedelta(seconds=seconds)
+        jte = parse_time(jet.end_time) + datetime.timedelta(seconds=seconds)
+        time_range = vso.attrs.Time(jts, jte)
 
         # Storage for the jet observables
         jet_observables = []
         for observable in observables:
             instrument = observable[0]
             measurement = observable[1]
-            z = client.query(times, instrument, measurement)
+            z = client.query(time_range, instrument, measurement)
             jet_observables.append(z)
             print(instrument, measurement)
             print('Number of files = %i' % len(z))
