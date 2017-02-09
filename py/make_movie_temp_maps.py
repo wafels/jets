596710319871bd88a1889e6c65d0d278fe50b8ac
Jack Ireland <jackireland@gmail.com>
2017-02-09 10:28:26 -0500

    improved movies, and image/movie saving

--- py/make_movie_temp_maps.py
+++ py/make_movie_temp_maps.py
@@ -6,11 +6,13 @@ import glob
 from sunpy.map import Map
 from astropy.visualization import LinearStretch, PercentileInterval
 from astropy.visualization.mpl_normalize import ImageNormalize
-import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import numpy as np
 import matplotlib as mpl
+
+
+output_directory = os.path.expanduser('~/jets/img/2012-11-20/jet_region_B_3')
+date_format = "%Y/%m/%d %H:%M:%S"
 
 
 # Normalize all the maps
@@ -58,15 +60,11 @@ mc = Map(file_list, cube=True)
 # Temperature bins used in the reconstruction
 temps = [0.5*1e6, 1.0*1e6, 2.0*1e6, 4.0*1e6, 6.0*1e6, 9.0*1e6, 14.0*1e6]
 # Boundaries
-boundaries = [0] + temps + [18.0 * 1e6]
+boundaries = temps
 cmap3 = mpl.colors.ListedColormap(['k', 'b', 'g', 'y', 'r', 'w'])
 cmap3.set_over('0.75')
 cmap3.set_under('0.35')
 norm3 = mpl.colors.BoundaryNorm(temps, cmap3.N)
-#cb3 = mpl.colorbar.ColorbarBase(axl3, cmap=cmap3, norm=norm3,
-#                                ticks=temps,  # optional
-#                                spacing='proportional')
-#cb3.set_label('MK')
 
 mc = movie_normalization(mc, percentile_interval=100.0)
 for i in range(0, len(mc)):
@@ -79,26 +77,26 @@ for i in range(0, len(mc)):
     mc[i].draw_limb()
     cbar = fig.colorbar(im, cmap=cmap3, ticks=temps, norm=norm3,
                         orientation='vertical', spacing='proportional',
-                        extend='both', boundaries=boundaries)
+                        boundaries=boundaries)
     cbar.ax.set_yticklabels(['0.5', '1.0', '2.0', '4.0', '6.0', '9.0', '14.0'])
     cbar.ax.set_ylabel('MK')
     #plt.colorbar(label='temperature (MK)')
-    plt.title('maximum temperature\n{:s}'.format(mc[i].date.strftime("%Y/%m/%d %H:%M:%S")))
-    plt.savefig('jet_dem_temp_{:n}.png'.format(i))
+    plt.title('maximum temperature\n{:s}'.format(mc[i].date.strftime(date_format)))
+    filepath = os.path.join(output_directory, 'jet_dem_temp_{:n}.png'.format(i))
+    plt.savefig(filepath)
 
 
-
-aaa
 def myplot(fig, ax, sunpy_map):
     p = sunpy_map.draw_limb()
     p = sunpy_map.draw_grid()
+    ax.set_title('maximum temperature\n{:s}'.format(sunpy_map.date.strftime(date_format)))
     #fig.colorbar(sunpy_map.plot())
     return p
 
 ani = mc.plot(plot_function=myplot)
 Writer = animation.writers['avconv']
 writer = Writer(fps=20, metadata=dict(artist='SunPy'), bitrate=18000)
-fname = os.path.join('ccc.mp4')
+fname = os.path.join(output_directory, 'maximum_temperature.mp4')
 ani.save(fname, writer=writer)
 
 
